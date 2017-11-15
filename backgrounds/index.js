d7decce5e12365a7f9b94ecdde5c208f94039878
mystith <pkm4evr@gmail.com>
2017-11-15 18:53:46 -0500

    Add files via upload

--- backgrounds/index.js
+++ backgrounds/index.js
@@ -1,3 +1,3 @@
 for(var i = 1; i < 33; i++){
-    document.getElementById('bgtablebody').innerHTML += "<p class='robotosmol outline'>background id: " + i + "</p><br><img class='background' src='/backgrounds/" + i + ".png' alt='bg' height='300' width='1040'><br>";
+    document.getElementById('mainbg').innerHTML += "<p class='robotosmol outline'>background id: " + i + "</p><br><img class='background' src='file:///C:/Users/mystith/Documents/mystith.github.io-master/backgrounds/" + i + ".png' alt='bg' height='300' width='1040'><br>";
 }
