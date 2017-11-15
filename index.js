322228dca758083dc4d7d5c8395ae3a975f255c1
mystith <pkm4evr@gmail.com>
2017-11-14 21:21:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 for(var i = 1; i < 32; i++){
-    document.getElementById('bgtablebody').innerHTML += "<hr width='100%'><p class='robotosmol'>Background ID:" + i + "</p><br><img src='mystith.github.io/backgrounds/" + i + ".png' alt='bg' height='300' width='1040'><br>";
+    document.getElementById('bgtablebody').innerHTML += "<p class='robotosmol'>Background ID:" + i + "</p><br><img src='mystith.github.io/backgrounds/" + i + ".png' alt='bg' height='300' width='1040'><hr width='100%'><br>";
 }
