6f668583f56adf3427d24d525e78b4daded92712
mystith <pkm4evr@gmail.com>
2017-12-31 00:14:06 -0500

    fixed js error
    again, title

--- nekozi/backgrounds/index.js
+++ nekozi/backgrounds/index.js
@@ -1,3 +1,3 @@
 for(var i = 1; i < 33; i++){
-    document.getElementById('mainbg').innerHTML += "<br><p class='robotosmol outlinelite'>background id: " + i + "</p><br><img class='background' src='/backgrounds/" + i + ".png' alt='bg' height='300' width='1040'><br>";
+    document.getElementById('mainbg').innerHTML += "<br><p class='robotosmol outlinelite'>background id: " + i + "</p><br><img class='background' src='/nekozi/backgrounds/" + i + ".png' alt='bg' height='300' width='1040'><br>";
 }
