f23d451cfaeb33865071b9314b4cfd8250384a57
mystith <mystith@gmail.com>
2017-12-30 22:03:21 -0500

    moved some stuff
    moved all nekozi-related stuff to it's own folder

--- backgrounds/index.js
+++
@@ -1,3 +0,0 @@
-for(var i = 1; i < 33; i++){
-    document.getElementById('mainbg').innerHTML += "<br><p class='robotosmol outline'>background id: " + i + "</p><br><img class='background' src='/backgrounds/" + i + ".png' alt='bg' height='300' width='1040'><br>";
-}
--- index.js
+++
@@ -1,3 +0,0 @@
-for(var i = 1; i < 32; i++){
-    document.getElementById('bgtablebody').innerHTML += "<p class='robotosmol'>Background ID:" + i + "</p><br><img src='mystith.github.io/backgrounds/" + i + ".png' alt='bg' height='300' width='1040'><hr width='100%'><br>";
-}
